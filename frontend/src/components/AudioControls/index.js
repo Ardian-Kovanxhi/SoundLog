a15f01182f549d1a9002b33eb76691ba87eed740
Ardian-Kovanxhi <akovanxhi@gmail.com>
2023-06-12 02:22:54 -0400

    audio player set up fully

--- frontend/src/components/AudioControls/index.js
+++ frontend/src/components/AudioControls/index.js
@@ -13,15 +13,25 @@ function AudioControls({ isLoaded }) {
     // const songs = useSelector(state => state.songs.allSongs[1].content)
     const song = useSelector(state => state.songs.playingSong)
 
-    return (
-        <AudioPlayer
-            className='audio-player'
-            autoPlay
-            src={song ? song.content : null}
-            onPlay={e => console.log("onPlay")}
+    const [playState, setPlayState] = useState(true)
 
-        // other props here
-        />
+    const playerClass = 'audio-player ' + (playState ? '' : 'invisible')
+
+    return (
+        <>
+            <button
+                className='test-button'
+                onClick={() => (playState ? setPlayState(false) : setPlayState(true))}
+            >test</button>
+            <AudioPlayer
+                className={playerClass}
+                autoPlay
+                src={song ? song.content : null}
+                onPlay={e => console.log("onPlay")}
+
+            // other props here
+            />
+        </>
     )
 }
 
