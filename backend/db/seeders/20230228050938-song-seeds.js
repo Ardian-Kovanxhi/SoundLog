6bc0b5ce0d44b12ed78517c46e4e38a09f30dc56
Ardian-Kovanxhi <akovanxhi@gmail.com>
2023-08-08 16:45:03 -0400

    seeder adjustment

--- backend/db/seeders/20230228050938-song-seeds.js
+++ backend/db/seeders/20230228050938-song-seeds.js
@@ -38,98 +38,98 @@ module.exports = {
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Instant Crush',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690301002405.mp3',
         img: 'https://upload.wikimedia.org/wikipedia/commons/7/74/Daft_Punk_-_Random_Access_Memories.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'SLOW DANCING IN THE DARK',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690301089615.mp3',
         img: 'https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/joji_press_image_sdp1l8/joji-north-american-tour-glimpse-of-us.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: "Since I've Been Loving You",
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690301222005.mp3',
         img: 'https://extrachill.com/wp-content/uploads/2021/03/led-zeppelin-angel-logo.jpeg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Stylo',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690301328111.mp3',
         img: 'https://m.media-amazon.com/images/I/71lix6+VfWL._UF1000,1000_QL80_.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Muscle Museum',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690301377211.mp3',
         img: 'https://m.media-amazon.com/images/I/71kN8laMZbL._UF1000,1000_QL80_.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Fuel',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690302075499.mp3',
         img: 'https://cdn.media.amplience.net/i/metallica/reload_cover?fmt=auto&maxW=1050',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Lady Evil',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690302291199.mp3',
         img: 'https://m.media-amazon.com/images/I/71y8IkqMGtL._UF1000,1000_QL80_.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: "Why Are Sunday's So Depressing",
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690302428705.mp3',
         img: 'https://m.media-amazon.com/images/I/91PbdrEjhaL._UF1000,1000_QL80_.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Pyramid Song',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690302506104.mp3',
         img: 'https://cdn.theatlantic.com/thumbor/1JDudCkfR7jwWW_hya_ZIGdLhV4=/444x138:2694x2388/1080x1080/media/img/mt/2021/06/artworks_AEUZPcuuIEgfqYbw_syqOSA_t3000x3000/original.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Layla',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690302647605.mp3',
         img: 'https://e.snmc.io/i/600/s/8ed126d701eb838b86808bdc87bc3922/1210092/derek-and-the-dominos-layla-and-other-assorted-love-songs-cover-art.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Heart Shaped Box',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690302694099.mp3',
         img: 'https://m.media-amazon.com/images/I/81o5RZfWzwL._UF1000,1000_QL80_.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Wish You Were Here',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690302769905.mp3',
         img: 'https://m.media-amazon.com/images/I/71m0ofUWYXL._UF1000,1000_QL80_.jpg',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Cubensis Lenses',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690303073398.mp3',
         img: 'https://f4.bcbits.com/img/a3335171517_65',
         description: null
       },
       {
-        userId: 2,
+        userId: 4,
         name: 'Moving Out',
         content: 'https://aa-sounclod-clone-bucket.s3.amazonaws.com/1690303153575.mp3',
         img: 'https://m.media-amazon.com/images/I/71cF66uUR4L._UF1000,1000_QL80_.jpg',
