e6a93bf8877a1198ef43bdec87faf90e6f03ae95
Ardian-Kovanxhi <akovanxhi@gmail.com>
2023-08-29 22:38:41 -0400

    playlist song planning

---
+++ backend/routes/api/playlistSongs.js
@@ -0,0 +1,13 @@
+/* 
+NEEDED ROUTES
+
+GET ROUTES
+-----------------------------------------------------------------------
+all playlist songs
+single playlist song (maybe)
+
+POST a playlist song
+PUT (edit) a playlist playlist song
+DELETE a playlist song (delete from playlist but not db BUT deleted songs are removed from playlist)
+
+*/
