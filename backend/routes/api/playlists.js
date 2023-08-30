b015b239d6cce380fa867cbffda57d5fd1f303f8
Ardian-Kovanxhi <akovanxhi@gmail.com>
2023-08-29 22:37:06 -0400

    playlist planning

---
+++ backend/routes/api/playlists.js
@@ -0,0 +1,14 @@
+/* 
+NEEDED ROUTES
+
+GET ROUTES
+-----------------------------------------------------------------------
+all playlists
+all user playlists
+single playlist
+
+POST a playlist
+PUT (edit) a playlist
+DELETE playlist
+
+*/
