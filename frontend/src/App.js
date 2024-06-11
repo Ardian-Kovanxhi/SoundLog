49b9489a352e81ecfe38886db52d7df8b73d0e07
Ardian-Kovanxhi <akovanxhi@gmail.com>
2024-06-11 13:49:50 -0400

    fixed minor errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,92 +18,94 @@ import { getLikesByUser } from "./store/likes";
 
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const playing = useSelector(state => state.songs.playingSong)
-  const pageState = useSelector(state => state.global.lightState)
-  const loadState = useSelector(state => state.global.loadState)
-  const User = useSelector(state => state.session.user)
+	const dispatch = useDispatch();
+	const [isLoaded, setIsLoaded] = useState(false);
+	const playing = useSelector(state => state.songs.playingSong)
+	const pageState = useSelector(state => state.global.lightState)
+	const loadState = useSelector(state => state.global.loadState)
+	const User = useSelector(state => state.session.user)
 
 
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-    if (!Cookies.get('pageTheme')) Cookies.set('pageTheme', 'day', { expires: 604800 })
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+		if (!Cookies.get('pageTheme')) Cookies.set('pageTheme', 'day', { expires: 604800 })
+	}, [dispatch]);
 
-  useEffect(() => {
-    if (User) dispatch(getLikesByUser());
-  }, [User])
+	useEffect(() => {
+		if (User) dispatch(getLikesByUser());
+	}, [User])
 
-  return (
+	return (
 
-    <div
-      className='page-container-div'
-      style={{
-        backgroundImage: `url(${pageState ? dayImage : nightImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: '50% 50%',
-        transition: 'background-image .5s ease'
-      }}
-    >
+		<div
+		className='page-container-div'
+		style={{
+			backgroundImage: `url(${pageState ? dayImage : nightImage})`,
+				backgroundSize: 'cover',
+				backgroundPosition: '50% 50%',
+				transition: 'background-image .5s ease'
+		}}
+		>
 
-      <div
-        className="loader"
-        style={{
-          zIndex: loadState ? 9 : 0
-        }}
-      >
-        <img
-          style={{
-            position: 'absolute',
-            opacity: pageState ? '100%' : 0,
-            transition: 'opacity .5s'
-          }}
-          src={dayLoad}
-        />
-        <img
-          // src="https://i.imgur.com/DwJvkT6.gif" 
-          // src={pageState ? dayLoad : nightLoad}
-          src={nightLoad}
-          style={{
-            opacity: pageState ? 0 : '100%',
-            transition: 'opacity .5s'
-          }}
-        />
-      </div>
+		<div
+		className="loader"
+		style={{
+			zIndex: loadState ? 9 : 0
+		}}
+		>
+		<img
+		style={{
+			position: 'absolute',
+				opacity: pageState ? '100%' : 0,
+				transition: 'opacity .5s'
+		}}
+		alt="loader"
+		src={dayLoad}
+		/>
+		<img
+		// src="https://i.imgur.com/DwJvkT6.gif" 
+		// src={pageState ? dayLoad : nightLoad}
+		src={nightLoad}
+		style={{
+			opacity: pageState ? 0 : '100%',
+				transition: 'opacity .5s'
+		}}
+		alt="loader"
+		/>
+		</div>
 
-      <div
-        className={`filler-color ${pageState ? '' : ' night'}`}
-        style={{
-          zIndex: loadState ? 8 : 0
-        }}
-      ></div>
+		<div
+		className={`filler-color ${pageState ? '' : ' night'}`}
+		style={{
+			zIndex: loadState ? 8 : 0
+		}}
+		></div>
 
-      {
-        playing.content ?
-          <div className={`bottom-filler ${pageState ? '' : ' night'}`}></div>
-          :
-          ''
-      }
+		{
+			playing.content ?
+			<div className={`bottom-filler ${pageState ? '' : ' night'}`}></div>
+			:
+			''
+		}
 
-      <Navigation isLoaded={isLoaded} />
-      {
-        isLoaded && (
-          <>
-            <Switch>
-              <Route path={'/songs/:songId'} component={SingleSong} />
-              <Route path={'/songs'} component={SongFormPage} />
-              <Route path={'/users/:userId'} component={UserPage} />
-              <Route exact path={'/'} component={SongSplash} />
-              <Route path={'*'} component={ErrorPage} />
-            </Switch>
-          </>
-        )
-      }
+		<Navigation isLoaded={isLoaded} />
+		{
+			isLoaded && (
+				<>
+				<Switch>
+				<Route path={'/songs/:songId'} component={SingleSong} />
+				<Route path={'/songs'} component={SongFormPage} />
+				<Route path={'/users/:userId'} component={UserPage} />
+				<Route exact path={'/'} component={SongSplash} />
+				<Route path={'*'} component={ErrorPage} />
+				</Switch>
+				</>
+			)
+		}
 
-      <AudioControls />
-    </div >
-  );
+		<AudioControls />
+		</div >
+	);
 }
 
 export default App;
